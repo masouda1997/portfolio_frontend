5c5c804284f7279c64ade233d6fb317a8acefcb8
masouda1997 <masoud30376@gmail.com>
2024-06-22 13:37:08 +0330

    add xpcart  component

---
+++ src/components/XPcards.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const XPcards = ({title , tag, company="",text, icon,color}) => {
+	return (
+		<div className="relative border-l-2 px-10 flex flex-col justify-betweens gap-3 items-start text-left ">
+			<span
+				style={{ backgroundColor: `${color}` }}
+				className="w-12 h-12 rounded-full flex justify-center items-center absolute -top-6 -left-6  "
+			>
+            {icon}
+			</span>
+			<span className="rounded-full bg-secondary bg-opacity-40 px-1 text-sm">
+				{tag}
+			</span>
+			<h3 className="tracking-tight uppercase text-lg ">
+				{title}
+				<span className="font-bold text-sm"> - {company}</span>{" "}
+			</h3>
+			<p>{text}</p>
+		</div>
+	);
+};
+
+export default XPcards;
