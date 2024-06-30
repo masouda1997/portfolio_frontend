ecdf2a5bbfbd2365cbe9b7720380e241ee1ff584
masouda1997 <masoud30376@gmail.com>
2024-06-30 10:11:12 +0330

    add active color for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import { CiHome } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
@@ -33,6 +34,8 @@ import { CiMail } from "react-icons/ci";
 // ];
 
 const Menu = () => {
+	const pathname = usePathname()
+	
    const [menuHover , setMenuHover] = useState({
       home:false,
       about:false,
@@ -43,22 +46,19 @@ const Menu = () => {
 		<ul className="flex  justify-center items-center flex-col gap-3 overflow-hidden">
 			<li
 				className=" flex items-center justify-center self-end "
-				onMouseEnter={() =>
-					setMenuHover((prev) => ({ ...prev, home: true }))
-				}
-				onMouseLeave={() =>
-					setMenuHover((prev) => ({ ...prev, home: false }))
-				}
+				onMouseEnter={() =>setMenuHover((prev) => ({ ...prev, home: true }))}
+				onMouseLeave={() =>setMenuHover((prev) => ({ ...prev, home: false }))}
 			>
 				<Link
 					style={{ border: `var(--primary-color) 1px solid` }}
-					className="p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)]"
+					className={`p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)] ${pathname === '/' && 'bg-[var(--primary-color)]'} `}
 					href={"/"}
 				>
 					{menuHover.home && <span className="pl-3 tracking-tight">HOME</span>}
 					<CiHome className="w-8 h-8 p-1" />
 				</Link>
 			</li>
+
 			<li
 				className="  flex items-center justify-center self-end "
 				onMouseEnter={() =>
@@ -70,13 +70,14 @@ const Menu = () => {
 			>
 				<Link
 					style={{ border: `var(--primary-color) 1px solid` }}
-					className="p-2 rounded-full flex items-center justify-center self-end hover:bg-[var(--primary-color)]"
+					className={`p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)] ${pathname === '/about' && 'bg-[var(--primary-color)]'} `}
 					href={"/about"}
 				>
 					{menuHover.user && <span className="pl-3 tracking-tight">ABOUT</span>}
 					<CiUser className="w-8 h-8 p-1" />
 				</Link>
 			</li>
+
 			<li
 				className=" flex items-center justify-center self-end "
 				onMouseEnter={() =>
@@ -88,13 +89,14 @@ const Menu = () => {
 			>
 				<Link
 					style={{ border: `var(--primary-color) 1px solid` }}
-					className="p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)]"
+					className={`p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)] ${pathname === '/portfolio' && 'bg-[var(--primary-color)]'} `}
 					href={"/portfolio"}
 				>
 					{menuHover.portfolio && <span className="pl-3 tracking-tight">PORTFOLIO</span>}
 					<CiFolderOn className="w-8 h-8 p-1" />
 				</Link>
 			</li>
+
 			<li
 				className="flex items-center justify-center self-end  "
 				onMouseEnter={() =>
@@ -106,7 +108,7 @@ const Menu = () => {
 			>
 				<Link
 					style={{ border: `var(--primary-color) 1px solid` }}
-					className="p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)]"
+					className={`p-2 rounded-full  flex items-center justify-center self-end hover:bg-[var(--primary-color)] ${pathname === '/contact' && 'bg-[var(--primary-color)]'} `}
 					href={"/contact"}
 				>
 					{menuHover.contact && <span className="pl-3 tracking-tight">CONTACT</span>}
