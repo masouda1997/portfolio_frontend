ecc925f66c142a8f65fd0e93e713013ca40966d6
masouda1997 <masoud30376@gmail.com>
2024-08-12 16:51:16 +0330

    trying to render menu dynamicly not fully handled

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,46 +7,75 @@ import { CiFolderOn } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
 import { CiBeaker1 } from "react-icons/ci";
 
-// const menuData = [
-// 	{
-// 		id: 1,
-// 		title: "Home",
-// 		link: "/home",
-// 		icon: 'CiHome',
-// 	},
-// 	{
-// 		id: 2,
-// 		title: "About",
-// 		link: "/about",
-// 		icon: 'CiUser',
-// 	},
-// 	{
-// 		id: 3,
-// 		title: "Portfolio",
-// 		link: "/portfolio",
-// 		icon: 'CiFolderOn',
-// 	},
-// 	{
-// 		id: 4,
-// 		title: "Contact",
-// 		link: "/contact",
-// 		icon: 'CiMail',
-// 	},
-// ];
+const menuData = [
+	{
+		id: 1,
+		title: "home",
+		link: "/",
+		icon: CiHome,
+	},
+	{
+		id: 2,
+		title: "about",
+		link: "/about",
+		icon: CiUser,
+	},
+	{
+		id: 3,
+		title: "portfolio",
+		link: "/portfolio",
+		icon: CiFolderOn,
+	},
+	{
+		id: 4,
+		title: "contact",
+		link: "/contact",
+		icon: CiMail,
+	},
+	{
+		id: 5,
+		title: "fun",
+		link: "/fun",
+		icon: CiBeaker1 ,
+	},
+];
 
-const Menu =() => {
-		const pathname = usePathname()
-		
-		const [menuHover , setMenuHover] = useState({
-			home:false,
-			about:false,
-			portfolio:false,
-			contact:false,
-			fun:false
-		})
-		return (
-			<ul className="flex justify-center items-center flex-col self-center gap-3 overflow-hidden">
+const Menu = () => {
+	const pathname = usePathname();
+
+	const [menuHover, setMenuHover] = useState({
+		home: false,
+		about: false,
+		portfolio: false,
+		contact: false,
+		fun: false,
+	});
+	return (
+		<ul className="flex justify-center items-center flex-col self-center gap-3 overflow-hidden">
+			{menuData.map((item) => (
 				<li
+					key={item.id}
+					className="flex items-center justify-center self-end"
+				>
+					<Link
+						style={{ border: `var(--primary-color) 1px solid` }}
+						className={`p-2 rounded-full flex items-center justify-center self-end hover:bg-[var(--primary-color)] ${
+							window.location.pathname === item.link &&
+							"bg-[var(--primary-color)]"
+						}`}
+						href={item.link}
+					>
+						{menuHover[item.title] && (
+							<span className="pl-3 tracking-tight uppercase">
+								{item.title}
+							</span>
+						)}
+						<item.icon className="w-8 h-8 p-1" />
+					</Link>
+				</li>
+			))}
+
+			{/* <li
 					className=" flex items-center justify-center self-end "
 					onMouseEnter={() =>setMenuHover((prev) => ({ ...prev, home: true }))}
 					onMouseLeave={() =>setMenuHover((prev) => ({ ...prev, home: false }))}
@@ -132,8 +161,8 @@ const Menu =() => {
 						{menuHover.fun && <span className="pl-3 tracking-tight">4 FUN </span>}
 						<CiBeaker1 className="w-8 h-8 p-1 " />
 					</Link>
-				</li>
-			</ul>
-		);
-	}
+				</li> */}
+		</ul>
+	);
+};
 export default Menu;
