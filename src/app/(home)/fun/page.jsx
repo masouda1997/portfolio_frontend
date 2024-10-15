7e081a8ae4a95d322231b196bbf7c5ba1b7f8b1f
masoud <masoud30376@gmail.com>
2024-10-15 16:30:54 +0330

    mouse trail image updated

--- src/app/(home)/fun/page.jsx
+++ src/app/(home)/fun/page.jsx
@@ -1,17 +1,35 @@
 "use client";
 import EmptySpace from "@/components/EmptySpace";
+import MouseImageTrail from "@/components/MouseImageTrail";
 import PageTitle from "@/components/PageTitle";
+import { floatingCard } from "@/constants";
 import React from "react";
 
 const page = () => {
 	return (
 		<section className="h-screen  relative flex flex-col items-center bg-transparent overflow-x-hidden no-scrollbar">
-         <PageTitle primaryColor={"var(--primary-color)"} mainText={'explore for'} colorText={'fun'} shadowText={'creativity'} />
+			<PageTitle
+				primaryColor={"var(--primary-color)"}
+				mainText={"explore for"}
+				colorText={"fun"}
+				shadowText={"creativity"}
+			/>
 
 			<section className="text-2xl  w-full min-h-screen">
-				<EmptySpace/>
+				<EmptySpace />
+				<MouseImageTrail
+					renderImageBuffer={50}
+					rotationRange={25}
+					images={floatingCard}
+				>
+					<section className="grid h-screen w-full place-content-center ">
+						<p className="flex items-center gap-2 text-3xl font-bold uppercase ">
+							<span>Hover me</span>
+						</p>
+					</section>
+				</MouseImageTrail>
 			</section>
-      </section>
+		</section>
 	);
 };
 
--- src/components/MouseImageTrail.jsx
+++ src/components/MouseImageTrail.jsx
@@ -1,12 +1,17 @@
-import { useAnimate } from 'framer-motion'
-import React from 'react'
+import { useAnimate } from "framer-motion";
+import React, { useRef } from "react";
 
-const MouseImageTrail = ({children , images , renderImageBuffer , rotationRange}) => {
-  const [scope, animate] = useAnimate()
-  const lastRenderPosition = useRef({ x: 0, y: 0 });
-  const imageRenderCount = useRef(0);
+const MouseImageTrail = ({
+	children,
+	images,
+	renderImageBuffer,
+	rotationRange,
+}) => {
+	const [scope, animate] = useAnimate();
+	const lastRenderPosition = useRef({ x: 0, y: 0 });
+	const imageRenderCount = useRef(0);
 
-  const handleMouseMove = (e) => {
+	const handleMouseMove = (e) => {
 		const { clientX, clientY } = e;
 		const distance = calculateDistance(
 			clientX,
@@ -14,37 +19,81 @@ const MouseImageTrail = ({children , images , renderImageBuffer , rotationRange}
 			lastRenderPosition.current.x,
 			lastRenderPosition.current.y
 		);
-    if (distance >= renderImageBuffer){
-      lastRenderPosition.current.x = clientX
-      lastRenderPosition.current.y = clientY
-      renderNextImage()
-    }
-  };
+		if (distance >= renderImageBuffer) {
+			lastRenderPosition.current.x = clientX;
+			lastRenderPosition.current.y = clientY;
+			renderNextImage();
+		}
+	};
 
-  const calculateDistance = (x1,y1,x2,y2)=>{
-    const deltaX = x2-x1
-    const deltaY = y2-y1
-    const distance  = Math.sqrt(deltaX**2 + deltaY**2)
-    return distance
-  }
+	const calculateDistance = (x1, y1, x2, y2) => {
+		const deltaX = x2 - x1;
+		const deltaY = y2 - y1;
+		const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+		return distance;
+	};
 
-  const renderNextImage = () =>{
-    
-  }
+	const renderNextImage = () => {
+		const imageIndex = imageRenderCount.current % images.length;
+		const selector = `[data-mouse-move-index="${imageIndex}"]`;
 
-  return (
-    <div className='relative overflow-hidden' ref={scope} onMouseMove={handleMouseMove}>
-      {children}
-      {images.map((img , index)=>(
-        <img 
-          key={index} 
-          src={img} 
-          className='pointer-events-none absolute left-0 top-0 h-48 w-auto rounded-xl border-2 border-black bg-neutral-900 object-cover opacity-0' 
-          alt={`Mouse move image ${index}`} 
-        />
-      ))}
-    </div>
-  )
-}
+		const el = document.querySelector(selector);
+    console.log(el);
+		el.style.top = `${lastRenderPosition.current.y}px`;
+		el.style.left = `${lastRenderPosition.current.x}px`;
+		el.style.zIndex = imageRenderCount.current.toString();
 
-export default MouseImageTrail
+		const rotation = Math.random() * rotationRange;
+
+		animate(
+			selector,
+			{
+				opacity: [0, 1],
+				transform: [
+					`translate(-50%, -25%) scale(0.5) ${
+						imageIndex % 2
+							? `rotate(${rotation}deg)`
+							: `rotate(-${rotation}deg)`
+					}`,
+					`translate(-50%, -50%) scale(1) ${
+						imageIndex % 2
+							? `rotate(-${rotation}deg)`
+							: `rotate(${rotation}deg)`
+					}`,
+				],
+			},
+			{ type: "spring", damping: 15, stiffness: 200 }
+		);
+
+		animate(
+			selector,
+			{
+				opacity: [1, 0],
+			},
+			{ ease: "linear", duration: 0.5, delay: 5 }
+		);
+
+		imageRenderCount.current += 1;
+	};
+
+	return (
+		<div
+			className="relative overflow-hidden"
+			ref={scope}
+			onMouseMove={handleMouseMove}
+		>
+			{children}
+			{images.map((img, index) => (
+				<img
+					key={index}
+					src={img.src}
+					className="pointer-events-none absolute left-0 top-0 h-48 w-auto rounded-xl border-2 border-black bg-neutral-900 object-cover opacity-0"
+					alt={`Mouse move image ${index}`}
+          data-mouse-move-index={index}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default MouseImageTrail;
